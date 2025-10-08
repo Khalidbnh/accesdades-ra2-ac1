aec61d95fb0674eb8632a1a894269350c5f1458b
KhalidBH <khalid-net2012@LIVE.COM>
2025-10-08 19:24:32 +0200

    Add Customer and Student model classes; update controllers and application properties

--- src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/controller/CustomerController.java
+++ src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/controller/CustomerController.java
@@ -1,11 +1,9 @@
 package accesdades.ra2.ac1.accesdades_ra2_ac1.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -14,7 +12,7 @@ public class CustomerController {
 
     @GetMapping("/customers")
     public String getCustomers() {
-        
+
         return "Endpoint GET Customers ";
     }
 
--- src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/controller/StudentController.java
+++ src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/controller/StudentController.java
@@ -3,11 +3,9 @@ package accesdades.ra2.ac1.accesdades_ra2_ac1.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController

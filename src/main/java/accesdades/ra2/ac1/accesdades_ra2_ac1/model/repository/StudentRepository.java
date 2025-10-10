5fca43f1b0b86104a3ed462e90a4acce62866299
KhalidBH <khalid-net2012@LIVE.COM>
2025-10-10 21:55:41 +0200

    add findAll and save in StudentRepository

--- src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/model/repository/StudentRepository.java
+++ src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/model/repository/StudentRepository.java
@@ -5,4 +5,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class StudentRepository {
 
+    public void findAll() {
+        
+    }
+
+    public void save() {
+        
+    }
+
 }

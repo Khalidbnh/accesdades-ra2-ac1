adf5fcccecc014a24f662c636d9bb6133a28588f
KhalidBH <khalid-net2012@LIVE.COM>
2025-10-15 19:58:37 +0200

    Add and ShowStudents on database

--- src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/controller/StudentController.java
+++ src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/controller/StudentController.java
@@ -4,6 +4,13 @@ package accesdades.ra2.ac1.accesdades_ra2_ac1.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import accesdades.ra2.ac1.accesdades_ra2_ac1.model.Student;
+import accesdades.ra2.ac1.accesdades_ra2_ac1.model.repository.StudentRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,13 +19,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/api")
 public class StudentController {
 
+    @Autowired
+    StudentRepository studentRepository;
+
     @GetMapping("/students")
-    public String getStudents() {
-        return "Endpoint GET Students";
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
     }
 
     @PostMapping("/students/batch")
     public String addStudent() {
-        return "Endpoint POST Students";
+        int numReg = studentRepository.save();
+        return "You added " + numReg + " student(s)";
     }
+
+    
 }
--- src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/model/repository/StudentRepository.java
+++ src/main/java/accesdades/ra2/ac1/accesdades_ra2_ac1/model/repository/StudentRepository.java
@@ -1,16 +1,46 @@
 package accesdades.ra2.ac1.accesdades_ra2_ac1.model.repository;
 
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.jdbc.core.RowMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
+
+import accesdades.ra2.ac1.accesdades_ra2_ac1.model.Student;
 
 @Repository
 public class StudentRepository {
 
-    public void findAll() {
-        
+    @Autowired 
+    JdbcTemplate jdbcTemplate;
+
+    
+    private static final class StudentRowMapper implements RowMapper<Student>{
+        @Override
+        public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Student student = new Student();
+            student.setId(rs.getLong("id"));
+            student.setName(rs.getString("name"));
+            student.setAge(rs.getInt("age"));
+            return student;
+        }
     }
 
-    public void save() {
-        
+
+    public List<Student> findAll() {
+        String sql = "SELECT * FROM Students";
+        return jdbcTemplate.query(sql, new StudentRowMapper());
+    }
+
+    public int save() {
+        String sql = "INSERT INTO Students (name, age) VALUES (?, ?)";
+        int numReg = jdbcTemplate.update(sql, "Khalid", 27);
+        return numReg;     
     }
 
 }
